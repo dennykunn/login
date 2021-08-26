cb1d8d95ecddf4561809f0ef7f2967146a8cb049
dennykunn <dennykunn@gmail.com>
2021-08-26 11:06:50 +0700

    pertama commit

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+<?php
+
+$koneksi = mysqli_connect("localhost", "root", "", "login");
+
+
+
+// Uji Koneksi
+// if($koneksi) {
+//     echo "aman";
+// } else{
+//     echo "gagal";
+// }
---
+++ main.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+
+    <title>Aplikasi Penjualan</title>
+</head>
+
+<body>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <div class="container">
+            <a class="navbar-brand" href="#">APLIKASI PENJUALAN</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">BERANDA <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">APLIKASI PENJUALAN</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">DATA USERS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">DATA BARANG</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">DATA PENJUALAN</a>
+                    </li>
+                </ul>
+                <div class="ml-auto"><a class="btn btn-danger" href="logout.php">LOGOUT</a></div>
+            </div>
+        </div>
+    </nav>
+
+    <!--  -->
+    <div class="bg-light border-bottom py-3">
+        <div class="container d-flex align-items-center">
+            <img src="images/keranjang.png" alt="Keranjang" width="50">
+            <p class="ml-2">Selamat Datang, <span class="font-weight-bold"><?= $_SESSION['nama']; ?></span> di Aplikasi Penjualan Rekayasa Perangkat Lunak <span class="font-weight-bold">(RPL SMK Taruna Persada)</span></p>
+        </div>
+    </div>
+
+    <!--  -->
+    <div class="container border mt-2 py-3 bg-light">
+        <h1>HALAMAN BERANDA</h1>
+    </div>
+
+
+
+
+
+
+    <!--  -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+
+</html>
