4bfd0b04744e649df6c7810efbc0cfbbe65f5c27
dennykunn <60263420+dennykunn@users.noreply.github.com>
2021-09-02 11:08:40 +0700

    Add files via upload

--- data_user.php
+++ data_user.php
@@ -1,10 +1,9 @@
 <h1>HALAMAN DATA USERS</h1>
 
-<a class="btn btn-success mt-1">Tambah Data User</a>
-
 <table class="table table-striped mt-4">
   <thead>
     <tr>
+      <a href="main.php?p=tambah_data_user" class="btn btn-success mt-1">Tambah Data User</a>
       <th scope="col">Username</th>
       <th scope="col">Password</th>
       <th scope="col">Nama Lengkap</th>
@@ -12,21 +11,21 @@
     </tr>
   </thead>
   <tbody>
-  <?php
+    <?php
     $query = mysqli_query($koneksi, "SELECT * FROM user");
-    while($data = mysqli_fetch_array($query)) {
-  ?>
-    <tr>
-      <td><?= $data['username']; ?></td>
-      <td><?= $data['password']; ?></td>
-      <td><?= $data['nama_lengkap']; ?></td>
-      <td>
-          <a class="btn btn-primary">Edit</a>
-          <a class="btn btn-danger">Delete</a>
-      </td>
-    </tr>
-  <?php
+    while ($data = mysqli_fetch_array($query)) {
+    ?>
+      <tr>
+        <td><?= $data['username']; ?></td>
+        <td><?= $data['password']; ?></td>
+        <td><?= $data['nama_lengkap']; ?></td>
+        <td>
+          <a href="main.php?p=edit_data_user&id=<?= $data['user_id']; ?>" class="btn btn-primary">Edit</a>
+          <a href="proses.php?aksi=delete&id=<?= $data['user_id']; ?>" class="btn btn-danger">Delete</a>
+        </td>
+      </tr>
+    <?php
     }
-  ?>
+    ?>
   </tbody>
 </table>
--- proses.php
+++ proses.php
@@ -15,8 +15,67 @@ if (isset($_POST['login'])) {
     if ($data) {
         $_SESSION['id'] = $data['user_id'];
         $_SESSION['nama'] = $data['nama_lengkap'];
+        // pindah ke halaman main.php
         header('location:main.php');
     } else {
         header('location:index.php?pesan=gagal');
     }
 }
+// batas proses login
+
+// proses tambah user
+if (isset($_POST['simpan_user'])) {
+    // Ambil data input (username, password, nama_lengkap)
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+    $namalengkap = $_POST['nama_lengkap'];
+
+    $query = mysqli_query($koneksi, "INSERT INTO user VALUES('','$username','$password','$namalengkap')");
+
+    if ($query) {
+        // pindah ke halaman main.php
+        header('location:main.php?p=data_user');
+    } else {
+        header('location:main.php?pesan=gagal');
+    }
+}
+// batas tambah user
+
+//proses edit user
+if (isset($_POST['edit_user'])) {
+    // Ambil data input (id, username, password, nama_lengkap)
+    $id = $_POST['id'];
+    $username = $_POST['username'];
+    $password = ($_POST['password']);
+    $namalengkap = $_POST['nama_lengkap'];
+
+    if ($password == "") {
+        $query = mysqli_query($koneksi, "UPDATE user SET 
+        username = '$username',
+        nama_lengkap = '$namalengkap'
+        WHERE user_id = '$id'");
+    } else {
+        $query = mysqli_query($koneksi, "UPDATE user SET 
+        username = '$username',
+        password = md5('$password'),
+        nama_lengkap = '$namalengkap'
+        WHERE user_id = '$id'");
+    }
+
+    if ($query) {
+        header('location:main.php?p=data_user');
+    }
+}
+// batas proses edit user
+
+// proses delete user
+if (isset($_GET['aksi'])) {
+    $id = $_GET['id'];
+
+    $query = mysqli_query($koneksi, "DELETE FROM user where user_id='$id'");
+
+    if ($query) {
+        header('location:main.php?p=data_user');
+    }
+}
+// batas proses delete user
